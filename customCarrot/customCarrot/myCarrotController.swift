3e78ead90074a8a64cbc26e28bc2272cca235be3
이용수 <>
2023-05-15 18:35:27 +0900

    이거까찌만 해놓고 가자

--- customCarrot/customCarrot/myCarrotController.swift
+++ customCarrot/customCarrot/myCarrotController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class myCarrotController : UIViewController{
 
+    @IBOutlet weak var tableview : UITableView!
+    let myExchange = ["관심목록", "판매내역", "구매내역", "중고거래 가계부"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableview.delegate = self
+        tableview.dataSource = self
+        
     }
 
+    // tableview func
+    
+        
+}
+
+extension myCarrotController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myExchange.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
 }
